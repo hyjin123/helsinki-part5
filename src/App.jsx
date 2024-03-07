8f89543ae27cba52df7b6ba03d5c0eaf96c7f275
Sean <seanhoyeonjin@gmail.com>
2024-03-07 13:39:19 -0500

    finished exercise 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,21 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedInUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
 
   const handleLogin = async (event) => {
@@ -21,6 +33,10 @@ const App = () => {
       const user = await loginService.login({ username, password });
       // make the user data persist even if page is refreshed
       window.localStorage.setItem("loggedInUser", JSON.stringify(user));
+
+      // set the token, so it can be used when submitting a new blog (making a post request)
+      blogService.setToken(user.token);
+
       setUser(user);
       setUsername("");
       setPassword("");
@@ -37,6 +53,21 @@ const App = () => {
     window.localStorage.clear();
     // set the user state to empty
     setUser(null);
+  };
+
+  const handleBlogSubmit = async (event) => {
+    event.preventDefault();
+    const result = await blogService.create({
+      title,
+      author,
+      url,
+    });
+
+    // re-set the blog list, so it shows when new blog is added right away
+    setBlogs([...blogs, result]);
+    setTitle("");
+    setAuthor("");
+    setUrl("");
   };
 
   if (user === null) {
@@ -71,12 +102,49 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
-      <h3>logged in as {user.name}</h3>
-      <button onClick={handleLogout}>logout</button>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      <div>
+        <h2>blogs</h2>
+        <h3>logged in as {user.name}</h3>
+        <button onClick={handleLogout}>logout</button>
+        <div>
+          <h2>Create a new blog</h2>
+          <form onSubmit={handleBlogSubmit}>
+            <div>
+              Title
+              <input
+                type="text"
+                value={title}
+                name="Title"
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </div>
+            <div>
+              Author
+              <input
+                type="text"
+                value={author}
+                name="Author"
+                onChange={(event) => setAuthor(event.target.value)}
+              />
+            </div>
+            <div>
+              URL
+              <input
+                type="text"
+                value={url}
+                name="url"
+                onChange={(event) => setUrl(event.target.value)}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
